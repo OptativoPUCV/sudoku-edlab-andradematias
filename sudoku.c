31c79438c422ccdebd3efdf937994edcd6d97dd1
repl.it user <replituser@example.com>
2020-10-12 10:48:33 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -56,7 +56,7 @@ List* get_adj_nodes(Node* n){
     for (i = 0 ; i < 9 ; i++) {      
       for (j = 0 ; j < 9 ; j++) {        
         if (n->sudo[i][j] == k) {          
-          return list;
+          printf("test\n");
         }
       }
     }
