fc235b2dda82242bd1cbb7b503617aea25e5a673
repl.it user <replituser@example.com>
2020-10-15 08:46:47 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -140,7 +140,7 @@ int is_final(Node* n){
 Node* DFS(Node* initial, int* cont){
   Stack* S = createStack();
   push(S, initial);
-  while (!is_empty((List *)S)) {
+  while (!is_empty(S)) {
     Node * n = top(S);
     pop(S);
     if (is_final(n)) {
